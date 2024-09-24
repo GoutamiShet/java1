8efa293e469661c6ce23de780954b94f27564a79
GoutamiShet <goutami.23cs041@sode-edu.in>
2024-09-24 15:56:58 +0530

    class Human

---
+++ G8.java
@@ -0,0 +1,32 @@
+class Human{
+    public void job(){
+        System.out.println("working professional");}}
+class Teacher extends Human{
+    public void job(){
+        System.out.println("Teacher");}}
+class Doctor extends Human{ 
+    public void job(){
+       System.out.println("Doctor");}}
+
+        
+    
+
+
+/**
+ *
+ * @author smv
+ */
+public class G8 {
+     public static void main(String[] args){
+        Human amar=new Human();
+        Teacher babu=new Teacher();
+        Doctor chandra=new Doctor();
+        Human dinesh=new Doctor();
+        amar.job();
+        babu.job();
+        chandra.job();
+        dinesh.job();}}
+        
+        
+    
+
