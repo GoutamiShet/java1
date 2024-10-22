0491aeb0183e52ff79a0c7d79a63e4c8129e3b11
GoutamiShet <goutami.23cs041@sode-edu.in>
2024-10-22 15:49:09 +0530

    Celcius to Farenheit

---
+++ G12.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class G12 {
+   public static void main(String[] args){
+     //  Scanner scanner=new Scanner(System.in);
+       float tempC=100f;
+       float tempF=0.0f;
+       tempF=tempC*(9.0f/5.0f)+32;
+       System.out.println("the temp in Fis "+tempF);
+   } 
+}
