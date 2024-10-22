98d4c5843d433d2d91cf45a71a3128368142da9d
GoutamiShet <goutami.23cs041@sode-edu.in>
2024-10-22 15:51:04 +0530

    default values

---
+++ G15.java
@@ -0,0 +1,12 @@
+
+public class G15 {
+    public static void main(String[] args){
+        System.out.println("Datatypes\tDefault\tMin\tmax");
+        System.out.println("byte\t"+(byte)0+"\t"+Byte.MIN_VALUE+"\t"+Byte.MAX_VALUE);
+        System.out.println("short\t"+(short)0+"\t"+Short.MIN_VALUE+"\t"+Short.MAX_VALUE);
+        System.out.println("int\t"+(int)0+"\t"+Integer.MIN_VALUE+"\t"+Integer.MAX_VALUE);
+        System.out.println("longt"+(long)0+"\t"+Long.MIN_VALUE+"\t"+Long.MAX_VALUE);
+        System.out.println("float\t"+(float)0+"\t"+Float.MIN_VALUE+"\t"+Float.MAX_VALUE);
+        System.out.println("double\t"+(double)0+"\t"+Double.MIN_VALUE+"\t"+Double.MAX_VALUE);
+    }
+}
